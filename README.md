01379a5e6c4e951844f033416f9e8265c81ff14f
Özgür Çoban <ozgurcoban@Ozgur-MacBook-Air.local>
2023-08-10 00:40:47 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+![image](https://github.com/ozgurcoban01/purejs-todolist/assets/102681848/79d8fd92-79d4-4b65-80e6-a506491d2914)
---
+++ script.js
@@ -0,0 +1,169 @@
+const todoInput = document.querySelector(".added-todo").querySelector("input");
+const todoAddButton = document.querySelector(".added-confirm");
+const waitingTodos = document.querySelector(".todos");
+const finishedTodos = document.querySelector(".finished-todos");
+
+let newTodos;
+let newTodosFinishedButton;
+let newTodosDeleteButton;
+let finishedTodosInput;
+
+todoAddButton.addEventListener("click", newTodoAdd);
+
+document.querySelector(".add-todo").addEventListener("keypress", (e) => {
+  if (e.key == "Enter") {
+    newTodoAdd();
+  }
+});
+
+let todos = undefined;
+
+if (localStorage.getItem("todos") == null) {
+  localStorage.setItem("todosId", 0);
+}
+
+if (localStorage.getItem("todosId") == 0) {
+  todos = [];
+} else {
+  todos = JSON.parse(localStorage.getItem("todos"));
+
+}
+
+
+
+
+function newTodoAdd() {
+  if (todoInput.value != "") {
+    createTodo()
+
+  }
+}
+
+async function changeTodo(id,title){
+  const newTodo={
+    title:title
+  }
+  console.log(JSON.stringify(newTodo))
+  await fetch(
+    "https://todolist-backend-odq2.onrender.com/api/todos/changeTodo/"+id
+  ,{
+    method:"PUT",
+    headers:{          
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body : JSON.stringify(newTodo)
+  }).then(todoInput.value = "").then(orderAfterFetch)
+
+}
+
+async function deleteTodo(id){
+
+
+  await fetch(
+    "https://todolist-backend-odq2.onrender.com/api/todos/deleteTodo/"+id
+  ,{
+    method:"DELETE",
+    headers:{          
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+  }).then(todoInput.value = "").then(orderAfterFetch)
+
+}
+
+async function setFinishedTodo(id){
+  const newTodoo={
+    status:false
+  }
+ 
+  await fetch(
+    "https://todolist-backend-odq2.onrender.com/api/todos/changeTodo/"+id
+  ,{
+    method:"PUT",
+    headers:{          
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body : JSON.stringify(newTodoo)
+  }).then(todoInput.value = "").then(orderAfterFetch)
+
+}
+
+async function createTodo(){
+  const newTodo={
+    title:todoInput.value,
+  }
+  await fetch(
+    "https://todolist-backend-odq2.onrender.com/api/todos/createTodo"
+  ,{
+    method:"POST",
+    headers:{          
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body : JSON.stringify(newTodo)
+  }).then(todoInput.value = "").then(orderAfterFetch)
+
+}
+
+async function orderAfterFetch() {
+  await fetch(
+    "https://todolist-backend-odq2.onrender.com/api/todos/getAllTodos"
+  ).then((res) => res.json()).then(
+    (todos)=>{
+      waitingTodos.innerHTML = "";
+      finishedTodos.innerHTML = "";
+      todos.map((todo) => {
+        if (todo.status) {
+          waitingTodos.innerHTML += `
+           <div class="todo">
+              <input class="todo-content" value="${todo.title}" todoid="${todo._id}" />
+              <div class="todo-adjust" todoid="${todo._id}">Ok</div>
+              <div class="todo-delete" todoid="${todo._id}">Del</div>
+            </div>
+          `;
+        } else {
+          finishedTodos.innerHTML += `
+          <div class="finished-todo">
+          <div class="finished-todo-content" todoid="${todo._id}">${todo.title}</div>
+          <div class="todo-delete" todoid="${todo._id}">Del</div>
+        </div>
+          `;
+        }
+      });
+
+      newTodos = document.querySelectorAll(".todo-content");
+      newTodosFinishedButton = document.querySelectorAll(".todo-adjust");
+      newTodosDeleteButton = document.querySelectorAll(".todo-delete");
+      finishedTodosInput = document.querySelectorAll(".todo-content");
+    
+      finishedTodosInput.forEach((button) => {
+        button.addEventListener("focusout", () => {
+          changeTodo(button.getAttribute("todoId"),button.value)
+        });
+      });
+    
+      newTodosFinishedButton.forEach((button) => {
+        button.addEventListener("click", () => {
+   
+          setFinishedTodo(button.getAttribute("todoId"))
+        }); 
+      });
+    
+      newTodosDeleteButton.forEach((button) => {
+        button.addEventListener("click", () => {
+          deleteTodo(button.getAttribute("todoId"))
+        });
+      });
+    }
+    
+  );
+
+
+ 
+
+
+}
+
+orderAfterFetch()
